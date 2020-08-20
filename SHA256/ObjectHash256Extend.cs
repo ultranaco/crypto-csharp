0222dd8c807be78712bff8e9d976a53419e3daad
ultranaco <alex@MXCDP4LP62.pricetravel.com.mx>
2020-08-19 20:46:36 -0500

    refactor: fix namespace to sha256

--- SHA256/ObjectHash256Extend.cs
+++ SHA256/ObjectHash256Extend.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace Falcon.Crypto.SHA256
+namespace Ultranaco.Crypto.SHA256
 {
   public static class ObjectHash256Extend
   {
